04b31db2fdbb8d5db9fd0e98f62aae847774ee9a
tominovak33 <tominovak33@gmail.com>
2015-06-04 14:41:55 +0100

    Exercise 12
    Finish exercise 12 - HTTP server that returns the data posted to
    it after uppercasing it

---
+++ learnyounode/exercise_12.js
@@ -0,0 +1,18 @@
+var http = require('http')
+var map = require('through2-map');
+
+var port = process.argv[2];
+
+var server = http.createServer(function (request, response) {
+    // request handling logic...
+    if (request.method != 'POST') {
+    	return res.end('only deal with post requests');
+    }
+    
+    request.pipe(map(function (request) {
+      return request.toString().toUpperCase();
+    })).pipe(response)
+
+});
+
+server.listen(port);
